3cd3222592981f141300f4801380d0346c052528
Xoje <dawxoje@gmail.com>
2023-01-13 15:32:40 +0100

    hot save

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,9 +3,7 @@ import React from 'react'
 export default function RootLayout ({ children }: { children: React.ReactNode }) {
   return (
     <html>
-      <head>
-        <title>algo</title>
-      </head>
+
       <body>
         <div>
           algo antes
