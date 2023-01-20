fac146d44dc5e8fce674b3adf3f4ed48929efddf
Xoje Laptop <dawxoje@gmail.com>
2023-01-20 10:47:08 +0100

    hot save

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,42 @@
+'use client'
+
 import styles from '../styles/Porfolio.module.scss'
 import './global.scss'
-import GalleryProjects from '../components/organisms/GalleryProjects'
-import React from 'react'
-import TimeLineStudies from '../components/organisms/TimeLineStudies'
-export default function HomePage () {
-  return (
-    <>
+import { GalleryProjects, AboutMe, SkillsAndExperience } from '../components/organisms'
+import dynamic from 'next/dynamic'
+
+const TimeLineStudies = dynamic(() => import('../components/organisms/TimeLineStudies'), { ssr: false })
+const HomePage = () =>
+  <>
+    <main className={styles.main}>
+      <AboutMe />
       <GalleryProjects />
-      <TimeLineStudies />
-      <main className={styles.main}>
-        <div className={styles.container}>
-          <div className={styles.left}>Left</div>
-          <div className={styles.right}>Right</div>
-        </div>
-      </main>
-    </>
-  )
-}
+      {/* <TimeLineStudies /> */}
+      <SkillsAndExperience />
+    </main>
+
+    <main className={styles.main}>
+      <div className={styles.container}>
+        <div className={styles.left}>Left</div>
+        <div className={styles.right}>Right</div>
+      </div>
+    </main>
+  </>
+export default HomePage
+/* useEffect(() => {
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach(entry => {
+        // console.log(entry)
+        if (entry.isIntersecting) {
+          console.log(entry)
+          entry.target.classList.add('show')
+        } else {
+          entry.target.classList.remove('show')
+        }
+      })
+    })
+
+    const hiddenElements = document.querySelectorAll('.hidden')
+    console.log(hiddenElements)
+    hiddenElements.forEach(el => observer.observe(el))
+  }) */
